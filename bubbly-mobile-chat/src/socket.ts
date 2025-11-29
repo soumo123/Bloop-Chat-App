99a681b079e87c0e2dea462fc394fe0ec40e5466
Soummya Biswas <soummyabiswas999@gmail.com>
2025-11-29 15:21:10 +0530

    Deploy test-2

--- bubbly-mobile-chat/src/socket.ts
+++ bubbly-mobile-chat/src/socket.ts
@@ -1,6 +1,8 @@
 import { io, Socket } from 'socket.io-client';
+const apiUrl = import.meta.env.VITE_API_SOCKET_URL;
 
-const SOCKET_URL = 'http://localhost:8000';
+
+const SOCKET_URL = apiUrl;
 
 const socket: Socket = io(SOCKET_URL, {
   autoConnect: true,
